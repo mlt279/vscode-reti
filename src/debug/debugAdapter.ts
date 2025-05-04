42994caad33989a5b12539ca3d78ace449fe0075
ml279 <mltpllch+github@gmail.com>
2025-05-04 09:58:53 +0200

    added cleaned up mock debug adapter into extension

--- src/debug/debugAdapter.ts
+++ src/debug/debugAdapter.ts
@@ -1,7 +1,42 @@
-import { ReTIDebugSession } from './retiDebug';
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
 
+import { MockDebugSession } from './mockDebug';
+
+import { promises as fs } from 'fs';
 import * as Net from 'net';
+import { FileAccessor } from './mockRuntime';
 
+/*
+ * debugAdapter.js is the entrypoint of the debug adapter when it runs as a separate process.
+ */
+
+/*
+ * Since here we run the debug adapter as a separate ("external") process, it has no access to VS Code API.
+ * So we can only use node.js API for accessing files.
+ */
+const fsAccessor:  FileAccessor = {
+	isWindows: process.platform === 'win32',
+	readFile(path: string): Promise<Uint8Array> {
+		return fs.readFile(path);
+	},
+	writeFile(path: string, contents: Uint8Array): Promise<void> {
+		return fs.writeFile(path, contents);
+	}
+};
+
+/*
+ * When the debug adapter is run as an external process,
+ * normally the helper function DebugSession.run(...) takes care of everything:
+ *
+ * 	MockDebugSession.run(MockDebugSession);
+ *
+ * but here the helper is not flexible enough to deal with a debug session constructors with a parameter.
+ * So for now we copied and modified the helper:
+ */
+
+// first parse command line arguments to see whether the debug adapter should run as a server
 let port = 0;
 const args = process.argv.slice(2);
 args.forEach(function (val, index, array) {
@@ -20,13 +55,14 @@ if (port > 0) {
 		socket.on('end', () => {
 			console.error('>> client connection closed\n');
 		});
-		const session = new ReTIDebugSession();
+		const session = new MockDebugSession(fsAccessor);
 		session.setRunAsServer(true);
 		session.start(socket, socket);
 	}).listen(port);
-}
-else {
-	const session = new ReTIDebugSession();
+} else {
+
+	// start a single session that communicates via stdin/stdout
+	const session = new MockDebugSession(fsAccessor);
 	process.on('SIGTERM', () => {
 		session.shutdown();
 	});
--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,17 @@ import { assembleFile, assembleLine } from './reti/assembler';
 import { stateToString } from './reti/retiStructure';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
+import * as Net from 'net';
+import { randomBytes } from 'crypto';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { platform } from 'process';
+import { ProviderResult } from 'vscode';
+import { MockDebugSession } from './debug/mockDebug';
+import { activateMockDebug, workspaceFileAccessor } from './debug/activateMockDebug';
+
+
+const runMode: 'external' | 'server' | 'namedPipeServer' | 'inline' = 'inline';
 
 let languageClient: LanguageClient | undefined = undefined;
 
@@ -226,8 +237,111 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(EmulateCommand, QuizCommand, RandomCommand, AssembleCommand, StopEmulationCommand, DisassembleCommand, DebugCommand);
+
+	switch (runMode) {
+		case 'server':
+			// run the debug adapter as a server inside the extension and communicate via a socket
+			activateMockDebug(context, new MockDebugAdapterServerDescriptorFactory());
+			break;
+
+		case 'namedPipeServer':
+			// run the debug adapter as a server inside the extension and communicate via a named pipe (Windows) or UNIX domain socket (non-Windows)
+			activateMockDebug(context, new MockDebugAdapterNamedPipeServerDescriptorFactory());
+			break;
+
+		case 'external': default:
+			// run the debug adapter as a separate process
+			activateMockDebug(context, new DebugAdapterExecutableFactory());
+			break;
+
+		case 'inline':
+			// run the debug adapter inside the extension and directly talk to it
+			activateMockDebug(context);
+			break;
+	}
+
+}
+class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	// The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
+	// Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
+
+	createDebugAdapterDescriptor(_session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): ProviderResult<vscode.DebugAdapterDescriptor> {
+		// param "executable" contains the executable optionally specified in the package.json (if any)
+
+		// use the executable specified in the package.json if it exists or determine it based on some other information (e.g. the session)
+		if (!executable) {
+			const command = "absolute path to my DA executable";
+			const args = [
+				"some args",
+				"another arg"
+			];
+			const options = {
+				cwd: "working directory for executable",
+				env: { "envVariable": "some value" }
+			};
+			executable = new vscode.DebugAdapterExecutable(command, args, options);
+		}
+
+		// make VS Code launch the DA executable
+		return executable;
+	}
 }
 
+class MockDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	private server?: Net.Server;
+
+	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+
+		if (!this.server) {
+			// start listening on a random port
+			this.server = Net.createServer(socket => {
+				const session = new MockDebugSession(workspaceFileAccessor);
+				session.setRunAsServer(true);
+				session.start(socket as NodeJS.ReadableStream, socket);
+			}).listen(0);
+		}
+
+		// make VS Code connect to debug server
+		return new vscode.DebugAdapterServer((this.server.address() as Net.AddressInfo).port);
+	}
+
+	dispose() {
+		if (this.server) {
+			this.server.close();
+		}
+	}
+}
+
+class MockDebugAdapterNamedPipeServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	private server?: Net.Server;
+
+	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+
+		if (!this.server) {
+			// start listening on a random named pipe path
+			const pipeName = randomBytes(10).toString('utf8');
+			const pipePath = platform === "win32" ? join('\\\\.\\pipe\\', pipeName) : join(tmpdir(), pipeName);
+
+			this.server = Net.createServer(socket => {
+				const session = new MockDebugSession(workspaceFileAccessor);
+				session.setRunAsServer(true);
+				session.start(<NodeJS.ReadableStream>socket, socket);
+			}).listen(pipePath);
+		}
+
+		// make VS Code connect to debug server
+		return new vscode.DebugAdapterNamedPipeServer(this.server.address() as string);
+	}
+
+	dispose() {
+		if (this.server) {
+			this.server.close();
+		}
+	}
+}
 
 // This method is called when your extension is deactivated
 export function deactivate() {
